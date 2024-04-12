11914c9faaad1d0d34101e64951937d08ea2e2da
Mykyta Konopelko <nik.sativa@gmail.com>
2024-04-12 13:32:50 +0200

    dependencies updated

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -16,7 +16,7 @@ let package = Package(
         .library(name: "EventDispatcherKitTestHelpers", targets: ["EventDispatcherKitTestHelpers"])
     ],
     dependencies: [
-        .package(url: "https://github.com/NikSativa/Threading.git", .upToNextMajor(from: "1.3.3")),
+        .package(url: "https://github.com/NikSativa/Threading.git", .upToNextMajor(from: "1.3.4")),
         .package(url: "https://github.com/NikSativa/SpryKit.git", .upToNextMajor(from: "2.2.2"))
     ],
     targets: [
